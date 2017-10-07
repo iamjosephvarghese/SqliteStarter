889fea5a1432e6248cb56b6a7cf13b6b0e3a457d
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-10-07 12:25:16 +0530

    Starter Code Added

---
+++ app/src/main/java/com/roundmelon/joseph/sqlitestarter/DBHandler.java
@@ -0,0 +1,17 @@
+package com.roundmelon.joseph.sqlitestarter;
+
+/**
+ * Created by Joseph on 07/10/17.
+ */
+
+public class DBHandler {
+
+
+    private static  final int dbVersion = 1;
+    private static final String dbName = "students_list";
+    private static final String tableName = "students";
+    private static final String nameTable = "name";
+    private static final String roll_noTable = "roll_no";
+    private static final String ageTable = "age";
+
+}
---
+++ app/src/main/java/com/roundmelon/joseph/sqlitestarter/Student.java
@@ -0,0 +1,12 @@
+package com.roundmelon.joseph.sqlitestarter;
+
+/**
+ * Created by Joseph on 07/10/17.
+ */
+
+public class Student {
+    String name;
+    int roll_no;
+    int age;
+
+}
